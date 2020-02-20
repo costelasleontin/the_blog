c318b49937fd87286e5cd464a6036a7f0786bc31
costelasleontin <costelasleontin@yahoo.com>
2020-02-20 22:56:43 +0200

    first commit

---
+++ build/precache-manifest.16338186915be67e680e23b8bfa84b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744e8825a97b25548cf72559f63c1435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59bbf0d306a9cf23f7b2",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "468a615ddfa725fffec8",
+    "url": "/static/css/main.53d00942.chunk.css"
+  },
+  {
+    "revision": "59bbf0d306a9cf23f7b2",
+    "url": "/static/js/2.7ad330c1.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.7ad330c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468a615ddfa725fffec8",
+    "url": "/static/js/main.6af8e135.chunk.js"
+  },
+  {
+    "revision": "7d3ff881c0ffbde66a6d",
+    "url": "/static/js/runtime-main.07c471aa.js"
+  }
+]);
