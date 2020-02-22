4c751b7d4f8046e3cb4b1baff7fd620797a8288b
costelasleontin <costelasleontin@yahoo.com>
2020-02-22 10:05:58 +0200

    Second commit

---
+++ src/components/Home.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from '../components/Home.js';
+
+test("Check if component renders",()=>{
+    // Will be implemented
+    //ReactDOM.render(<Home/>,document.getElementById('root'))
+});
+
