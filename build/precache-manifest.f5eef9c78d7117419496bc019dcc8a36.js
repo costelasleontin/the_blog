911ead7bc65b6f3b67d58d765c35068c1096d638
costelasleontin <costelasleontin@yahoo.com>
2020-02-22 21:08:53 +0200

    Heroku deploy

---
+++ build/precache-manifest.f5eef9c78d7117419496bc019dcc8a36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e66bee22afe3425d5957b0b360668cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89330ad12a456c6d6bfd",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "774262e840462aa34f25",
+    "url": "/static/css/main.53d00942.chunk.css"
+  },
+  {
+    "revision": "89330ad12a456c6d6bfd",
+    "url": "/static/js/2.acebd824.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.acebd824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774262e840462aa34f25",
+    "url": "/static/js/main.c194f136.chunk.js"
+  },
+  {
+    "revision": "7d3ff881c0ffbde66a6d",
+    "url": "/static/js/runtime-main.07c471aa.js"
+  }
+]);
